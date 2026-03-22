124ccfadd57f5f937d410e2970e912567fec0125
Gold <152667851+goldscore@users.noreply.github.com>
2026-03-22 14:16:35 -0400

    Delete sw.js

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-importScripts("/assets/scramjet/scramjet.sync.js");
-
-const scramjet = new ScramjetServiceWorker();
-
-self.addEventListener("fetch", (event) => {
-  if (scramjet.route(event)) {
-    event.respondWith(scramjet.fetch(event));
-  }
-});
